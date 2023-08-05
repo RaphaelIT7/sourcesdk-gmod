7224d56aecbb0860f208eb099d20acbfa86a4bf9
RaphaelIT7 <raphael.i.t@outlook.de>
2023-08-05 16:47:08 +0200

    Added some functions. ToDo: Add all missing files.

--- game/server/gameinterface.h
+++ game/server/gameinterface.h
@@ -57,6 +57,11 @@ public:
 
 	// Notify that the player is spawned
 	virtual void			ClientSpawned( edict_t *pPlayer ) OVERRIDE;
+
+	// ToDo: Test if these could belong to IServerGameClients.
+	virtual void			GMOD_ClientConnected(int);
+	virtual void 			GMOD_SentClientStringTables(int);
+	virtual void			GMOD_ReceiveClientMessage(int, edict_t*, bf_read*, int);
 };
 
 
@@ -152,6 +157,11 @@ public:
 
 	// Called to see if the game server is okay with a manual changelevel or map command
 	virtual bool			IsManualMapChangeOkay( const char **pszReason ) OVERRIDE;
+
+	// ToDo: Test if these could belong to IServerGameClients.
+	virtual void 			GMOD_OnAllSoundsStoppedSV();
+	virtual void 			GMOD_ClientSignOnStateChanged( int, int, int );
+	virtual bool 			GMOD_CheckPassword( long long, char const*, char const*, char const*, char const*, char *,uint );
 
 private:
 
