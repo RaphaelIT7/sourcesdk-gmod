79a2476bc490901ccbccea18d4cce70dda10e1b6
RaphaelIT7 <raphael.i.t@outlook.de>
2023-08-08 21:53:12 +0200

    Added gmcommon. Love it + gonna need it later.

--- gmod/premake5.lua
+++ gmod/premake5.lua
@@ -6,6 +6,7 @@ function IncludeSDKEngine()
 	local _project = project()
 
 	externalincludedirs(current_dir .. "/../gmod")
+	externalincludedirs(current_dir .. "/../gmcommon_helper")
 
 	filter({})
 end
